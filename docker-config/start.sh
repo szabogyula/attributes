c250e0d96607d5e3c0f5d5be0c2a2eee8fdb7360
Szabó Gyula <gyufi@sztaki.hu>
2024-01-18 22:52:02 +0000

    Update mod_auth_mellon.conf and start.sh

--- docker-config/start.sh
+++ docker-config/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 if [ $1 == 'shibboleth' ]; then
   cp -a /etc/shibboleth_confd /etc/confd
@@ -7,11 +8,14 @@ if [ $1 == 'shibboleth' ]; then
   a2ensite default-ssl
   exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
 else
-  cp -a /etc/mellon_confd /etc/confd
-  # /usr/local/bin/confd -onetime -backend env || { echo 'confd failed' ; exit 1; }
+  sed -i "s#http:\/\/localhost:8081#https://${VIRTUAL_HOST}#g" /etc/mellon/mellon_metadata.xml
+  mkdir /var/www/html/mellon/sp
+  cp -a /etc/mellon/mellon_metadata.xml /var/www/html/mellon/sp/metadata.xml
   
+  sed -i "s#\# ServerName.*#ServerName https://${VIRTUAL_HOST}#g" /etc/apache2/sites-available/mod_auth_mellon.conf
+  sed -i "s#\# MellonDiscoveryURL.*#MellonDiscoveryURL ${DISCOVERY_URL}#g" /etc/apache2/sites-available/mod_auth_mellon.conf
+
   curl -o /etc/mellon/metadata/idp.xml ${IDP_METADATA_URL:-http://mdr/idp.xml}
-  ls -l /etc/mellon/metadata/
 
   a2dismod shib
   a2disconf shib
