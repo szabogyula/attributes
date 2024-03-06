f7a6d819d114be3fca129acb2a150485b9190770
Gyula Szabó <gyufi@sztaki.hu>
2024-03-06 22:03:14 +0100

    Update start.sh DISCOVERY_URL is optional

--- docker-config/start.sh
+++ docker-config/start.sh
@@ -13,8 +13,10 @@ else
   cp -a /etc/mellon/mellon_metadata.xml /var/www/html/mellon/sp/metadata.xml
   
   sed -i "s#\# ServerName.*#ServerName https://${VIRTUAL_HOST}#g" /etc/apache2/sites-available/mod_auth_mellon.conf
-  sed -i "s#\# MellonDiscoveryURL.*#MellonDiscoveryURL ${DISCOVERY_URL}#g" /etc/apache2/sites-available/mod_auth_mellon.conf
-
+  if [[ -n ${DISCOVERY_URL} ]]; then
+    sed -i "s#\# MellonDiscoveryURL.*#MellonDiscoveryURL ${DISCOVERY_URL}#g" /etc/apache2/sites-available/mod_auth_mellon.conf
+  endif
+  
   curl -o /etc/mellon/metadata/idp.xml ${IDP_METADATA_URL:-http://mdr/idp.xml}
 
   a2dismod shib
