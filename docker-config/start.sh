3dae2dfa2f2a59df8586b697725758609c78407a
gyufi <gyufi@sztaki.hu>
2024-08-15 12:46:18 +0200

    ..

--- docker-config/start.sh
+++ docker-config/start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+export PROTOCOL=http
+if [ -z "${NOSSL}" ]; then
+  export PROTOCOL=https
+fi
+
 if [ $1 == 'shibboleth' ]; then
   cp -a /etc/shibboleth_confd /etc/confd
   # /usr/local/bin/confd -onetime -backend env || { echo 'confd failed' ; exit 1; }
@@ -8,13 +13,16 @@ if [ $1 == 'shibboleth' ]; then
   a2ensite default-ssl
   exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
 else
-  sed -i "s#http:\/\/localhost:8081#https://${VIRTUAL_HOST}#g" /etc/mellon/mellon_metadata.xml
+  sed -i "s#http:\/\/localhost:8081#${PROTOCOL}://${VIRTUAL_HOST}#g" /etc/mellon/mellon_metadata.xml
   mkdir /var/www/html/mellon/sp
   cp -a /etc/mellon/mellon_metadata.xml /var/www/html/mellon/sp/metadata.xml
   
-  sed -i "s#\# ServerName.*#ServerName https://${VIRTUAL_HOST}#g" /etc/apache2/sites-available/mod_auth_mellon.conf
-  sed -i "s#\# MellonDiscoveryURL.*#MellonDiscoveryURL ${DISCOVERY_URL}#g" /etc/apache2/sites-available/mod_auth_mellon.conf
-
+  sed -i "s#\# ServerName.*#ServerName ${PROTOCOL}://${VIRTUAL_HOST}#g" /etc/apache2/sites-available/mod_auth_mellon.conf
+  
+  if [ -n "${DISCOVERY_URL}" ]; then
+    sed -i "s#\# MellonDiscoveryURL.*#MellonDiscoveryURL ${DISCOVERY_URL}#g" /etc/apache2/sites-available/mod_auth_mellon.conf 
+  fi
+  
   curl -o /etc/mellon/metadata/idp.xml ${IDP_METADATA_URL:-http://mdr/idp.xml}
 
   a2dismod shib
